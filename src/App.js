1af69fef70c8b5ad287cb1b4168f5064e02a580a
Arkadiusz Binkiewicz <arek.binkiewicz11@onet.pl>
2023-06-16 21:10:00 +0200

    currency converter without style and interactions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="form">
+    <fieldset className="form__fieldset">
+      <legend className="form__legend">Kalkulator walutowy</legend>
+      <p>
+        <label>
+          <span className="form__labelText">Kwota:</span>
+        </label>
+        <input className="form__field" type="number" step="1" min="1" name="amount" required/>
+      </p>
+      <p>
+        <label>
+          <span className="form__labelText">Waluta:</span>
+        </label>
+        <select className="form__field" name="currency">
+          <option name="EUR">EUR</option>
+          <option name="USD">USD</option>
+        </select>
+      </p>
+      <p className="form__paragraph">Wartość w PLN: <strong className=""></strong></p>
+      <button className="form__button ">Przelicz</button>
+    </fieldset>
+  </form>
   );
 }
 
