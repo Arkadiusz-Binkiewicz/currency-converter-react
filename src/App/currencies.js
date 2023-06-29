a93ff90f0dba817bbf035a5dab7931ff0f29aa2f
Arkadiusz Binkiewicz <arek.binkiewicz11@onet.pl>
2023-06-29 21:56:50 +0200

    currencies added

---
+++ src/App/currencies.js
@@ -0,0 +1,12 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.0836,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.4397,
+    },
+];
