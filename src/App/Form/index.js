a55624bd75d83c94227d8b2a5b2bfcfea605c305
Arkadiusz Binkiewicz <arek.binkiewicz11@onet.pl>
2023-06-29 20:05:43 +0200

    added new style css and new file for Form and App

---
+++ src/App/Form/index.js
@@ -0,0 +1,31 @@
+import 
+
+const Form = () => {
+
+    return (
+        <form className="form">
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">Kalkulator walutowy</legend>
+                <p>
+                    <label>
+                        <span className="form__labelText">Kwota:</span>
+                    </label>
+                    <input className="form__field" type="number" step="1" min="1" name="amount" required />
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">Waluta:</span>
+                    </label>
+                    <select className="form__field" name="currency">
+                        <option name="EUR">EUR</option>
+                        <option name="USD">USD</option>
+                    </select>
+                </p>
+                <p className="form__paragraph">Wartość w PLN: <strong className=""></strong></p>
+                <button className="form__button ">Przelicz</button>
+            </fieldset>
+        </form>
+    );
+};
+
+export default Form;
